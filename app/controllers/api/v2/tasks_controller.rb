4c22ff5c31679ca2e18cca1b047c3be701387a2c
Danilo de Carvalho Souza <danilotremere@gmail.com>
2017-10-23 01:30:21 +0000

    Implementing JSONAPI.ORG patterns for tasks

--- app/controllers/api/v2/tasks_controller.rb
+++ app/controllers/api/v2/tasks_controller.rb
@@ -4,7 +4,7 @@ class Api::V2::TasksController < ApplicationController
   def index
     begin
       tasks = current_user.tasks
-      render json: { tasks: tasks }, status: 200
+      render json: tasks, status: 200
     rescue
       head 404
     end
